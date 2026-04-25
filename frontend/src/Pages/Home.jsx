a9b13564ac02628e02a87c5ee2ba165d72315f06
Shubham Sharma <shubhamss1403@gmail.com>
2026-04-25 16:27:57 +0530

    update

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Components/Header'
-import Speciality from '../Components/speciality'
+import Speciality from '../Components/Speciality'
 import TopDoctors from '../Components/TopDoctors'
 import Banner from '../Components/Banner'
 
